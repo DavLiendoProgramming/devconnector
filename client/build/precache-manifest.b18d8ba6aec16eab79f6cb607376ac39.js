f44c73e3595faba82644a89ae9e943eb019cbeb6
David Liendo <dliendo05@gmail.com>
2020-06-04 13:16:33 -0400

    react-script bug fix

---
+++ client/build/precache-manifest.b18d8ba6aec16eab79f6cb607376ac39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6278369b9b5783bdfc127488237c49ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0fb1df9e3d198b620fd",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0da8e16cf50a1ad21d8b",
+    "url": "/static/css/main.a4bcdcf2.chunk.css"
+  },
+  {
+    "revision": "b0fb1df9e3d198b620fd",
+    "url": "/static/js/2.12645c3e.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.12645c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da8e16cf50a1ad21d8b",
+    "url": "/static/js/main.d6088fb3.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
